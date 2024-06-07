37fe0fb6c04d5712914c6a24aa6a068db42e1cc9
Matthew Wells <mattwells9@shaw.ca>
2024-06-07 15:31:23 -0500

    fixed issue in parallel processing writes

--- src/data_scheduler.go
+++ src/data_scheduler.go
@@ -77,7 +77,7 @@ func buckets_indices(data_length int, bucket_size int) [][]int {
 
 
 
-func thread_execution(data_slice *[]*Profile, waitgroup * sync.WaitGroup, profile_compare *Profile, start_idx int, end_idx int, dist_fn func(*[]int, *[]int) float64, f *bufio.Writer){
+func thread_execution(data_slice *[]*Profile, waitgroup * sync.WaitGroup, profile_compare *Profile, start_idx int, end_idx int, dist_fn func(*[]int, *[]int) float64, array_writes *[]*string){
 	/* Compute profile differences.
 	
 	data_slice: the data range to use for calculation against the profile to be compared too.
@@ -86,24 +86,24 @@ func thread_execution(data_slice *[]*Profile, waitgroup * sync.WaitGroup, profil
 	start_idx: The starting range in the profile to be used to initilize comparisons
 	end_idx: The end range to calculate comparisons up too.
 	dist_fn: The distance function to use for calculation of differences. Takes pointer to two profile to compare and returns a float 64
+	array_writes: Array of values to append writes too
 	*/
 
-	truncate := distance_functions[DIST_FUNC].truncate
-	defer f.Flush()
 
+	var format_expression string = "%s %s %.2f\n";
+	if distance_functions[DIST_FUNC].truncate {
+		format_expression = "%s %s %.0f\n";
+	}
+
+	defer waitgroup.Done()
 	// TODO need to pass a slice properly in the future
 	for i := start_idx; i < end_idx; i++ {
 		x := dist_fn((*data_slice)[i].profile, profile_compare.profile);
 
-		if truncate {
-			fmt.Fprintf(f, "%s %s %.0f\n", profile_compare.name, (*data_slice)[i].name, x)
-		}else {
-			fmt.Fprintf(f, "%s %s %.2f\n", profile_compare.name, (*data_slice)[i].name, x)
-		}
+		output := fmt.Sprintf(format_expression, profile_compare.name, (*data_slice)[i].name, x);
+		(*array_writes)[i-start_idx] = &output;
 	}
-
-	defer waitgroup.Done()
-
+	
 }
 
 
@@ -125,17 +125,25 @@ func run_data(profile_data *[]*Profile, f *bufio.Writer) {
 	buckets := buckets_indices(len(data), bucket_size)
 	arr_pos := 1
 
-
 	for g := range data[0:] {
 		var wg sync.WaitGroup
 		profile_comp := data[g] // copy struct for each thread
+		values_write := make([]*[]*string, len(buckets) - bucket_index)
 		// TODO an incredible optimization here would be to go lockless, or re-use threads
 		for i := bucket_index; i < len(buckets); i++ {
 			wg.Add(1)
-			go thread_execution(&data, &wg, profile_comp, buckets[i][0], buckets[i][1], dist, f)
+			array_writes := make([]*string, buckets[i][1] - buckets[i][0])
+			go thread_execution(&data, &wg, profile_comp, buckets[i][0], buckets[i][1], dist, &array_writes)
+			values_write[i - bucket_index] = &array_writes
 		}
 		wg.Wait() // Wait for everyone to catch up
 		buckets[bucket_index][0]++ // update the current buckets tracker
+		
+		for _, i := range values_write {
+			for _, value := range *i {
+				fmt.Fprintf(f, *value);
+			}
+		}
 
 		if len(buckets) > 1 && arr_pos % bucket_size == 0 {
 			for f := buckets[bucket_index][1] - bucket_size; f < buckets[bucket_index][1]; f++ {
@@ -151,5 +159,6 @@ func run_data(profile_data *[]*Profile, f *bufio.Writer) {
 		}
 		arr_pos++;
 	}
+	defer f.Flush();
 }
 

ad284f4965af703d409840fae576bd5ab4601ed9
Matthew Wells <mattwells9@shaw.ca>
2024-06-19 09:32:28 -0500

    added test files

---
+++ pdists_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+
+
+// Tests for cli
+func Test_cli(t *testing.T){
+
+	
+	cli()
+	if distance_matrix.Used {
+		t.Error("Distance matrix should not have been used.")
+	}
+
+	if convert_matrix.Used {
+		t.Error("Convert matrix should not have been used.")
+	}
+
+	if fast_match.Used {
+		t.Error("Fast match should not have been used.")
+	}
+
+	if tree.Used {
+		t.Error("tree should not have been used.")
+	}
+}
